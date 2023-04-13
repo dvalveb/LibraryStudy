66e1907cc76e10c517c87dfe812388e79445a84f
dvalveb <130635052+dvalveb@users.noreply.github.com>
2023-04-13 17:59:19 +0900

    base Stream Library
    Stream class base code

---
+++ CoreLibrary/CoreLibrary/Stream/Stream.cpp
@@ -0,0 +1,144 @@
+﻿#include "Stream.h"
+
+CStream::CStream(VOID)
+{
+}
+
+CStream::~CStream(VOID)
+{
+}
+
+BOOL CStream::SetBuffer(BYTE* pucBuf)
+{
+	return 0;
+}
+
+BOOL CStream::SetBuffer()
+{
+	return 0;
+}
+
+BOOL CStream::ReadInt32(INT* piData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadDWORD(DWORD* pdwData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadDWORD_PTR(DWORD_PTR* pdwData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadByte(BYTE* pucData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadBytes(BYTE* pucData, DWORD dwLen)
+{
+	return 0;
+}
+
+BOOL CStream::ReadFloat(FLOAT* pfData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadInt64(INT64* pi64Data)
+{
+	return 0;
+}
+
+BOOL CStream::ReadUSHORT(USHORT* pusData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadSHORT(SHORT* psData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadBOOL(BOOL* pbData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadWCHAR(WCHAR* pwcData)
+{
+	return 0;
+}
+
+BOOL CStream::ReadWCHARs(LPWSTR lpwData, DWORD dwLen)
+{
+	return 0;
+}
+
+BOOL CStream::WriteInt32(INT iData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteDWORD(DWORD dwData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteDWORD_PTR(DWORD_PTR dwData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteByte(BYTE ucData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteBytes(BYTE* pucData, DWORD dwLen)
+{
+	return 0;
+}
+
+BOOL CStream::WriteFloat(FLOAT fData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteInt64(INT64 i64Data)
+{
+	return 0;
+}
+
+BOOL CStream::WriteUSHORT(USHORT usData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteSHORT(SHORT sData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteBOOL(BOOL bData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteWCHAR(WCHAR wcData)
+{
+	return 0;
+}
+
+BOOL CStream::WriteWCHARs(LPCWSTR lpcwData, DWORD dwLen)
+{
+	return 0;
+}
+
+DWORD CStream::GetLength(VOID)
+{
+	return 0;
+}
