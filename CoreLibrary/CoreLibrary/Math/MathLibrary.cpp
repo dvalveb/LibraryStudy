98d38f2b78a752271a89ed2cd13fc35d8e517b19
dvalveb <130635052+dvalveb@users.noreply.github.com>
2023-04-13 15:17:35 +0900

    add math library

--- CoreLibrary/CoreLibrary/Math/MathLibrary.cpp
+++ CoreLibrary/CoreLibrary/Math/MathLibrary.cpp
@@ -1,1 +1,21 @@
 ﻿#include "MathLibrary.h"
+
+double MathLbrary::ArithMatic::Add(double a, double b)
+{
+	return a + b;
+}
+
+double MathLbrary::ArithMatic::Sub(double a, double b)
+{
+	return a - b;
+}
+
+double MathLbrary::ArithMatic::Multiply(double a, double b)
+{
+	return a * b;
+}
+
+double MathLbrary::ArithMatic::Divide(double a, double b)
+{
+	return a / b;
+}
--- CoreLibrary/CoreLibrary/Math/MathLibrary.h
+++ CoreLibrary/CoreLibrary/Math/MathLibrary.h
@@ -1,1 +1,15 @@
 ﻿#pragma once
+namespace MathLbrary
+{
+	class ArithMatic
+	{
+	public:
+		static double Add(double a, double b);
+
+		static double Sub(double a, double b);
+
+		static double Multiply(double a, double b);
+
+		static double Divide(double a, double b);
+	};
+}
