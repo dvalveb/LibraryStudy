e573e40b629f0eeec278b8f1209a7fa8854e06f9
dvalveb <130635052+dvalveb@users.noreply.github.com>
2023-04-13 18:38:02 +0900

    Library regitry funtion
    registry function creattion basic

---
+++ CoreLibrary/CoreLibrary/registry/registry.cpp
@@ -0,0 +1,54 @@
+﻿#include "registry.h"
+
+CRegistry::CRegistry(VOID)
+{
+}
+
+CRegistry::~CRegistry(VOID)
+{
+}
+
+BOOL CRegistry::open(HKEY hRootKey, LPWSTR lpwSubKey)
+{
+	return 0;
+}
+
+BOOL CRegistry::close()
+{
+	return 0;
+}
+
+BOOL CRegistry::CreateKey(HKEY hRootKey, LPWSTR lpwSubKey)
+{
+	return 0;
+}
+
+BOOL CRegistry::DeleteKey(HKEY hRootKey, LPWSTR lpwSubKey)
+{
+	return 0;
+}
+
+BOOL CRegistry::SetValue(LPWSTR lpwValueName, LPWSTR lpwValue)
+{
+	return 0;
+}
+
+BOOL CRegistry::SetValue(LPWSTR lpwValueName, DWORD dwValue)
+{
+	return 0;
+}
+
+BOOL CRegistry::GetValue(LPWSTR lpwValueName, LPWSTR lpwValue, LPDWORD lpdwBufLen)
+{
+	return 0;
+}
+
+BOOL CRegistry::GetValue(LPWSTR lpwValueName, LPDWORD lpdwValue)
+{
+	return 0;
+}
+
+BOOL CRegistry::DeleteValue(LPWSTR lpwValueName)
+{
+	return 0;
+}
---
+++ CoreLibrary/CoreLibrary/registry/registry.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "../base/study_defs.h"
+
+class CRegistry
+{
+public:
+	CRegistry(VOID);
+	~CRegistry(VOID);
+
+	BOOL open(HKEY hRootKey, LPWSTR lpwSubKey);
+	BOOL close();
+
+	BOOL CreateKey(HKEY hRootKey, LPWSTR lpwSubKey);
+	BOOL DeleteKey(HKEY hRootKey, LPWSTR lpwSubKey);
+
+	BOOL SetValue(LPWSTR lpwValueName, LPWSTR lpwValue);
+	BOOL SetValue(LPWSTR lpwValueName, DWORD dwValue);
+
+	BOOL GetValue(LPWSTR lpwValueName, LPWSTR lpwValue, LPDWORD lpdwBufLen);
+	BOOL GetValue(LPWSTR lpwValueName, LPDWORD lpdwValue);
+
+	BOOL DeleteValue(LPWSTR lpwValueName);
+	
+private:
+};
