5c41c4629f0f42f3fce358748af825ef6a65cdd6
dvalveb <130635052+dvalveb@users.noreply.github.com>
2023-04-19 16:20:43 +0900

    create random code
    added random code

---
+++ CoreLibrary/CoreLibrary/random/random.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "../base/study_defs.h"
+
+class CRandom
+{
+public:
+	CRandom(UINT uiSeed);
+	~CRandom(VOID);
+
+	INT		Rand(VOID);
+	INT		SimpleRandom(VOID);
+	INT		ComplexRandom(VOID);
+	
+private:
+	UINT	m_uiSeed;
+};
